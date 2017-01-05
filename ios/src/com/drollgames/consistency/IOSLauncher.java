abf3156acbece73fe79add3d36367cf615cdd3c3
dhr039 <dima.hristov@gmail.com>
2017-01-05 13:34:43 +0200

    iOS: show interstitial, not very correct though (doing load inside the show);

--- ios/src/com/drollgames/consistency/IOSLauncher.java
+++ ios/src/com/drollgames/consistency/IOSLauncher.java
@@ -3,14 +3,23 @@ package com.drollgames.consistency;
 import org.robovm.apple.foundation.Foundation;
 import org.robovm.apple.foundation.NSAutoreleasePool;
 import org.robovm.apple.uikit.UIApplication;
+import org.robovm.apple.uikit.UIViewController;
+import org.robovm.pods.google.mobileads.GADInterstitial;
+import org.robovm.pods.google.mobileads.GADRequest;
 
 import com.badlogic.gdx.backends.iosrobovm.IOSApplication;
 import com.badlogic.gdx.backends.iosrobovm.IOSApplicationConfiguration;
 import com.drollgames.consistency.CJMain;
 
-public class IOSLauncher extends IOSApplication.Delegate {
+import java.util.Arrays;
+
+public class IOSLauncher extends IOSApplication.Delegate implements IActivityRequestHandler {
+
+    private static final String ADMOB_INTERSTITIAL_ID = "ca-app-pub-6925635541075684/6327090666";
 
     private static final String TAG = "DHRlogs ";
+    UIViewController viewController;
+    GADInterstitial adInterstitial;
 
     @Override
     protected IOSApplication createApplication() {
@@ -24,12 +33,44 @@ public class IOSLauncher extends IOSApplication.Delegate {
         config.orientationPortrait = true;
         config.useAccelerometer = false;
         config.useCompass = false;
-        return new IOSApplication(new CJMain(null), config);
+        return new IOSApplication(new CJMain(IOSLauncher.this), config);
     }
 
     public static void main(String[] argv) {
         NSAutoreleasePool pool = new NSAutoreleasePool();
         UIApplication.main(argv, null, IOSLauncher.class);
         pool.close();
+    }
+
+    public void loadIntersitial() {
+//        Foundation.log(TAG + "loadIntersitial");
+        viewController  = UIApplication.getSharedApplication().getKeyWindow().getRootViewController();
+        String interstitialId = ADMOB_INTERSTITIAL_ID;
+        adInterstitial = new GADInterstitial(interstitialId);
+
+        GADRequest request = new GADRequest();
+        // Display test ads on the simulator.
+        request.setTestDevices(Arrays.asList(GADRequest.getSimulatorID()));
+
+        adInterstitial.loadRequest(request);
+    }
+
+    @Override
+    public void showIntersitial() {
+        Foundation.log(TAG + "showIntersitial");
+        if(adInterstitial == null) {
+            loadIntersitial();
+            return;
+        }
+        if (adInterstitial.isReady()) {
+            if(viewController == null) {
+                loadIntersitial();
+                return;
+            }
+            adInterstitial.present(viewController);
+            loadIntersitial();
+        } else {
+            this.loadIntersitial();
+        }
     }
 }
