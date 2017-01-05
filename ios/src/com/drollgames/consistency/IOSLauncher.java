371ae54a44dd4cac96703e329a2ebba795273981
dhr039 <dima.hristov@gmail.com>
2017-01-05 12:04:37 +0200

    made the iOS version to work;

--- ios/src/com/drollgames/consistency/IOSLauncher.java
+++ ios/src/com/drollgames/consistency/IOSLauncher.java
@@ -1,5 +1,6 @@
 package com.drollgames.consistency;
 
+import org.robovm.apple.foundation.Foundation;
 import org.robovm.apple.foundation.NSAutoreleasePool;
 import org.robovm.apple.uikit.UIApplication;
 
@@ -8,10 +9,22 @@ import com.badlogic.gdx.backends.iosrobovm.IOSApplicationConfiguration;
 import com.drollgames.consistency.CJMain;
 
 public class IOSLauncher extends IOSApplication.Delegate {
+
+    private static final String TAG = "DHRlogs ";
+
     @Override
     protected IOSApplication createApplication() {
+//        IOSApplicationConfiguration config = new IOSApplicationConfiguration();
+//        return new IOSApplication(new CJMain(null), config);
+
+        Foundation.log(TAG + "createApplication");
+
         IOSApplicationConfiguration config = new IOSApplicationConfiguration();
-        return new IOSApplication(new CJMain(), config);
+        config.orientationLandscape = false;
+        config.orientationPortrait = true;
+        config.useAccelerometer = false;
+        config.useCompass = false;
+        return new IOSApplication(new CJMain(null), config);
     }
 
     public static void main(String[] argv) {
